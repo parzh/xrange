5215a0e5f58687e37e0be3b47b35beb30fb413f3
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2020-04-07 23:29:33 +0300

    Add JSDocs for test entities

--- test/entities.ts
+++ test/entities.ts
@@ -1,4 +1,7 @@
 export const REASONABLY_LARGE_NUMBER = 5245;
 
+/** `NaN`, `null`, and non-numeric values (except for functions) */
 export const nanofs: unknown[] = [ null, NaN, "text", "42", {} ];
+
+/** `NaN`, `null`, and non-numeric values (including functions) */
 export const nans: unknown[] = [ ...nanofs, (() => {}) ];
