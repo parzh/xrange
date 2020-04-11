f33a0e7b3d0fd4be9757fe0870078ac684a0fec6
Dima Parzhitsky <parzhitsky@gmail.com>
2020-04-12 00:02:29 +0300

    Address non-zero start

--- src/xrange-numeric.ts
+++ src/xrange-numeric.ts
@@ -2,9 +2,11 @@ import type XRange from "./typings/xrange";
 
 /** @internal */
 export default function* xrangeNumeric(start: number, stop: number, step: number): XRange {
+	const isUp = stop > start;
+
 	for (
 		let curr = start;
-		stop > 0 ? curr < stop : curr > stop;
+		isUp ? curr < stop : curr > stop;
 		curr += step
 	)
 		yield curr;
