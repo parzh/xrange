604704303749e0f0d289f2c46a82def303d4e991
Dima Parzhitsky <parzhitsky@gmail.com>
2020-04-20 20:22:21 +0300

    Assert full error message, but without error code

--- test/errors.spec.ts
+++ test/errors.spec.ts
@@ -55,21 +55,21 @@ describe("removeLastStackEntry(error)", () => {
 
 describe("createError(code, preserveStack?)", () => {
 	it("should create errors by given error code", () => {
-		expect(createError("XRANGE:_:UNKUSG").message).toMatch(/unknown usage encountered$/);
-		expect(createError("XRANGE:_:NOIMPL").message).toMatch(/feature is not implemented$/);
-		expect(createError("XRANGE:0:ARGREQ").message).toMatch(/argument is required$/);
-		expect(createError("XRANGE:1:ARGNAN").message).toMatch(/argument is not a number$/);
-		expect(createError("XRANGE:2:STPNAN").message).toMatch(/argument `stop` is not a number$/);
-		expect(createError("XRANGE:2:STRINF").message).toMatch(/argument `start` must be finite$/);
-		expect(createError("XRANGE:2:STRNAN").message).toMatch(/argument `start` is not a number$/);
-		expect(createError("XRANGE:3:BD1INF").message).toMatch(/range start \(first argument\) must be finite$/);
-		expect(createError("XRANGE:3:BD1NAN").message).toMatch(/argument `bound1` is not a number$/);
-		expect(createError("XRANGE:3:BD2INF").message).toMatch(/range start \(second argument\) must be finite$/);
-		expect(createError("XRANGE:3:BD2NNF").message).toMatch(/argument `bound2` is neither a number, nor a function$/);
-		expect(createError("XRANGE:3:STEINF").message).toMatch(/argument `step` must be finite$/);
-		expect(createError("XRANGE:3:STENAN").message).toMatch(/argument `step` is not a number$/);
-		expect(createError("XRANGE:3:STEZER").message).toMatch(/argument `step` cannot be zero$/);
-		expect(createError("XRANGE:3:NXTNAF").message).toMatch(/argument `next` is not a function$/);
+		expect(createError("XRANGE:_:UNKUSG").message).toMatch(/\] unknown usage encountered$/);
+		expect(createError("XRANGE:_:NOIMPL").message).toMatch(/\] feature is not implemented$/);
+		expect(createError("XRANGE:0:ARGREQ").message).toMatch(/\] argument is required$/);
+		expect(createError("XRANGE:1:ARGNAN").message).toMatch(/\] argument is not a number$/);
+		expect(createError("XRANGE:2:STPNAN").message).toMatch(/\] argument `stop` is not a number$/);
+		expect(createError("XRANGE:2:STRINF").message).toMatch(/\] argument `start` must be finite$/);
+		expect(createError("XRANGE:2:STRNAN").message).toMatch(/\] argument `start` is not a number$/);
+		expect(createError("XRANGE:3:BD1INF").message).toMatch(/\] range start \(first argument\) must be finite$/);
+		expect(createError("XRANGE:3:BD1NAN").message).toMatch(/\] argument `bound1` is not a number$/);
+		expect(createError("XRANGE:3:BD2INF").message).toMatch(/\] range start \(second argument\) must be finite$/);
+		expect(createError("XRANGE:3:BD2NNF").message).toMatch(/\] argument `bound2` is neither a number, nor a function$/);
+		expect(createError("XRANGE:3:STEINF").message).toMatch(/\] argument `step` must be finite$/);
+		expect(createError("XRANGE:3:STENAN").message).toMatch(/\] argument `step` is not a number$/);
+		expect(createError("XRANGE:3:STEZER").message).toMatch(/\] argument `step` cannot be zero$/);
+		expect(createError("XRANGE:3:NXTNAF").message).toMatch(/\] argument `next` is not a function$/);
 	});
 
 	it("should remove last stack entry by default", () => {
