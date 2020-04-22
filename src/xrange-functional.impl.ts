7d99ea5a7614910292a8aabb55c23caa770ec946
Dima Parzhitsky <parzhitsky@gmail.com>
2020-04-22 22:15:14 +0300

    Define `maxPrevLength` argument

--- src/xrange-functional.impl.ts
+++ src/xrange-functional.impl.ts
@@ -5,6 +5,6 @@ import type NextFactory from "./typings/next-factory";
 import { createError } from "./errors";
 
 /** @internal */
-export default function xrangeFunctional(start: number, predicate: Predicate, next: NextFactory): XRange {
+export default function xrangeFunctional(start: number, predicate: Predicate, next: NextFactory, maxPrevLength?: number): XRange {
 	throw createError("XRANGE:_:NOIMPL");
 }
