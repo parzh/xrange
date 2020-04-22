7896c117bfa738e4358b1c08aacd18e046d1d9ee
Dima Parzhitsky <parzhitsky@gmail.com>
2020-04-22 20:32:41 +0300

    Remove mutative array methods from `prev`
    Prevents some of the typing errors

--- src/typings/prev.ts
+++ src/typings/prev.ts
@@ -1,3 +1,3 @@
-export default interface Prev extends Partial<number[]> {
+export default interface Prev extends Partial<readonly number[]> {
 	0: number; // FIXME: looks weird, I know, but type [ number, ...number[] ] didn't work
 }
