f8817d4e5749c2a81cd9a494b3a2007e070332fb
Dima Parzhitsky <parzhitsky@gmail.com>
2020-04-23 18:53:00 +0300

    Cast arguments to number is numeric impl to match functional impl

--- src/xrange-numeric.impl.ts
+++ src/xrange-numeric.impl.ts
@@ -2,12 +2,14 @@ import type XRange from "./typings/xrange";
 
 /** @internal */
 export default function* xrangeNumeric(start: number, stop: number, step: number): XRange {
-	const isUp = step > 0;
+	const _stop = +stop;
+	const _step = +step;
+	const isUp = _step > 0;
 
 	for (
-		let curr = start;
-		isUp ? curr < stop : curr > stop;
-		curr += step
+		let curr = +start;
+		isUp ? curr < _stop : curr > _stop;
+		curr += _step
 	)
 		yield curr;
 }
