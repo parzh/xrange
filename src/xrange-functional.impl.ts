aaebeccc987f5883a98b1413adbd5d797f72ddcd
Dima Parzhitsky <parzhitsky@gmail.com>
2020-04-23 20:27:27 +0300

    Implement functional implementation

--- src/xrange-functional.impl.ts
+++ src/xrange-functional.impl.ts
@@ -1,10 +1,26 @@
 import type XRange from "./typings/xrange";
 import type Predicate from "./typings/predicate";
 import type NextFactory from "./typings/next-factory";
-
-import { createError } from "./errors";
+import type { Prev } from "./typings/next-factory";
 
 /** @internal */
-export default function xrangeFunctional(start: number, predicate: Predicate, next: NextFactory, maxPrevLength = Infinity): XRange {
-	throw createError("XRANGE:_:NOIMPL");
+export default function* xrangeFunctional(start: number, predicate: Predicate, next: NextFactory, maxPrevLength = Infinity): XRange {
+	const prevNeeded = maxPrevLength > 0 && (predicate.length >= 2 || next.length >= 1);
+
+	const prev: number[] = [];
+
+	let curr = +start;
+
+	while (predicate(curr, prev)) {
+		yield curr;
+
+		if (prevNeeded) {
+			prev.unshift(curr);
+
+			if (prev.length > maxPrevLength)
+				prev.pop();
+		}
+
+		curr = +next(prev as Prev);
+	}
 }
