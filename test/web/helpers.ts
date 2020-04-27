9d173f4780adc96d72177dbd8c950b0d8d28c559
Dima Parzhitsky <parzhitsky@gmail.com>
2020-04-27 13:17:02 +0300

    Delegate web setup to Jest environment

--- test/web/helpers.ts
+++ test/web/helpers.ts
@@ -2,29 +2,8 @@
  * @jest-environment jsdom
  */
 
-import { existsSync, readFileSync } from "fs";
-import { resolve } from "path";
-import { execSync } from "child_process";
-
-/** @private */
-const testBundlePath = resolve(__dirname, "xrange.bundle.js");
-
 declare global {
 	const xrange: typeof import("../../src");
 }
 
-export async function loadBundle(done: jest.DoneCallback): Promise<void> {
-	if (!existsSync(testBundlePath))
-		execSync("npm run build:web:test", { timeout: 10000, stdio: "ignore" });
-
-	const script = document.createElement("script");
-
-	script.onload = () => {
-		script.onload = null;
-		done();
-	};
-
-	script.innerHTML = readFileSync(testBundlePath, "utf-8");
-
-	document.head.append(script);
-}
+export {};
--- test/web/xrange.web.spec.ts
+++ test/web/xrange.web.spec.ts
@@ -2,12 +2,6 @@
  * @jest-environment jsdom
  */
 
-import { loadBundle } from "./helpers";
-
-jest.setTimeout(20000);
-
-beforeAll(loadBundle);
-
 it("should create global `xrange` object", async () => {
 	expect(typeof xrange).toBe("function");
 });
