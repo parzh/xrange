fe2e37c96a267d6cd2df620d112e1476312c61b3
Dima Parzhitsky <parzhitsky@gmail.com>
2020-05-14 16:04:47 +0300

    Completely wipe out unimplemented signature

--- src/typings/predicate.ts
+++
@@ -1,3 +0,0 @@
-export default interface Predicate {
-	(next: number, memo: number[]): boolean;
-}
--- src/xrange-looplike.impl.ts
+++ src/xrange-looplike.impl.ts
@@ -1,6 +1,6 @@
 import type XRange from "./typings/xrange";
-import type Predicate from "./typings/predicate";
-import type NextFactory from "./typings/next-factory";
+type Predicate = unknown; // TODO: define
+type NextFactory = unknown; // TODO: define
 
 /** @internal */
 declare function xrangeLooplike(start: number, predicate: Predicate, next: NextFactory): XRange; // TODO: export default
