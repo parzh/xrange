888e1b90ef3f1a51ef03f1a08fb99fa694f8e4f9
Dima Parzhitsky <parzhitsky@gmail.com>
2020-05-17 12:01:39 +0300

    Remove tests

--- test/build/entities.ts
+++
@@ -1,7 +0,0 @@
-import { resolve } from "path";
-
-export const distPath = resolve(__dirname, "../../dist");
-
-export const entryPath = resolve(distPath, "index.js");
-
-export const bundlePath = resolve(distPath, "xrange.bundle.js");
