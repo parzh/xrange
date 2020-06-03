0ce57228f211620d3cd8ee605ac6be77bc123220
Dima Parzhitsky <parzhitsky@gmail.com>
2020-06-03 13:06:45 +0300

    Re-export types from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import xrange from "./xrange";
 
+// @ts-ignore // FIXME: https://github.com/microsoft/TypeScript/issues/38866
 export = xrange;
+
+export type { default as XRange } from "./typings/xrange";
+export type { Predicate, NextFactory } from "@xrange/func";
