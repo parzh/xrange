d05523eb83102a8212606d8787c1d2358f1ed346
Dima Parzhitsky <parzhitsky@gmail.com>
2020-06-03 13:03:47 +0300

    Update deprecation notice

--- src/typings/next-factory.ts
+++ src/typings/next-factory.ts
@@ -2,9 +2,11 @@
 export type Memo = [ number, ...(number | undefined)[] ];
 
 /**
- * @deprecated Unused anywhere, left for backwards compatibility. Use `NextFactory` from `@xrange/func` instead
- * @example
+ * @deprecated Unused anywhere, left for backwards compatibility.
+ * Use `NextFactory` from `@xrange/func` instead:
+ * ```
  * import type { NextFactory } from "@xrange/func";
+ * ```
  */
 export default interface NextFactory {
 	(memo: Readonly<Memo>): number;
