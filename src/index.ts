a35d42630c4af5ee2766323a8bf641f572f2a8cf
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2020-09-12 13:18:58 +0300

    Don't re-export deprecated type

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import xrange from "./xrange";
 // @ts-ignore
 export = xrange; /*! FIXME: https://github.com/microsoft/TypeScript/issues/38866 (use `skipLibCheck: true` for now) */
 
-export type { default as XRange } from "./typings/xrange";
+export type { XRange } from "./xrange";
 export type { Predicate, NextFactory } from "@xrange/func";
