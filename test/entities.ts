b4cb886d80b4f2eda0c7adf7c1b0fe8266913b8b
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2020-09-12 13:05:01 +0300

    Remove unused constant

--- test/entities.ts
+++ test/entities.ts
@@ -1,5 +1,3 @@
-export const REASONABLY_LARGE_NUMBER = 5184;
-
 /** `NaN`, `null`, and non-numeric values (except for functions) */
 export const nanofs: unknown[] = [ null, NaN, "text", "42", true, false, Symbol("non-numeric"), {} ];
 
